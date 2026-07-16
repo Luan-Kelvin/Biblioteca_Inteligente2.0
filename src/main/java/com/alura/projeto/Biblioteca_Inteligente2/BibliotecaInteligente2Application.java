08383dedc6830d00533c7808d704f4c209b86d6a
Luan Kelvin <kelvinluan897@gmail.com>
2026-07-15 23:02:34 -0300

    first commit

---
+++ src/main/java/com/alura/projeto/Biblioteca_Inteligente2/BibliotecaInteligente2Application.java
@@ -0,0 +1,13 @@
+package com.alura.projeto.Biblioteca_Inteligente2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaInteligente2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaInteligente2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/projeto/Biblioteca_Inteligente2/BibliotecaInteligente2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.projeto.Biblioteca_Inteligente2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaInteligente2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
